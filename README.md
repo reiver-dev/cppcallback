7460629f13aa8a5ce0ab3e286b62bf7b32295ef9
Reiver <serious33@yandex.ru>
2013-10-12 18:55:30 +0400

    Callback refactoring, StaticClojure added

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ cppcallback
 Typesafe c++ delegates without dynamic memory allocation
 
 C++ 11 version of [Elbert Mai's CPPCallback](http://www.codeproject.com/Articles/136799/Lightweight-Generic-C-Callbacks-or-Yet-Another-Del)
+
+StaticClojure as simple custom size std::function
--- callback.hpp
+++ callback.hpp
@@ -1,161 +1,123 @@
-#ifndef UTIL_CALLBACK_HPP
-#define UTIL_CALLBACK_HPP
+#ifndef CALLBACK_HPP_
+#define CALLBACK_HPP_
 
-#define UTIL_GET_CALLBACK_FACTORY_BIND_FREE(freeFuncPtr) \
-    (callback_detail::GetCallbackFactory(freeFuncPtr).Bind<freeFuncPtr>())
-#define BIND_FREE_CB UTIL_GET_CALLBACK_FACTORY_BIND_FREE
+#include <utility>
 
-#define UTIL_GET_CALLBACK_FACTORY_BIND_MEMBER(memFuncPtr, instancePtr) \
-    (callback_detail::GetCallbackFactory(memFuncPtr).Bind<memFuncPtr>(instancePtr))
-#define BIND_MEM_CB UTIL_GET_CALLBACK_FACTORY_BIND_MEMBER
+namespace CB {
 
-
-namespace callback_detail {
-
-template<typename FuncSignature>
+template<typename Signature>
 class Callback;
 
-struct NullCallback {};
-
-template<typename R, typename ...Args>
-class Callback<R (Args...)>
-{
+template<typename R, typename ... ARGS>
+class Callback<R(ARGS...)> {
 public:
-    typedef R ReturnType;
 
-    Callback()                    : func(0), obj(0) {}
-    Callback(NullCallback)        : func(0), obj(0) {}
-    Callback(const Callback& rhs) : func(rhs.func), obj(rhs.obj) {}
-    ~Callback() {}
+	typedef R (sig)(ARGS...);
+	typedef R (*function)(void *, ARGS...);
 
-    Callback& operator=(NullCallback)
-        { obj = 0; func = 0; return *this; }
-    Callback& operator=(const Callback& rhs)
-        { obj = rhs.obj; func = rhs.func; return *this; }
+	Callback() :
+			m_obj(0),
+			m_func(0) {
+	}
 
-    inline R operator()(Args... arg) const
-    {
-        return (*func)(obj, arg...);
-    }
+	Callback(void *obj, function func) :
+			m_obj(obj),
+			m_func(func) {
+	}
+
+	Callback(const Callback &o) :
+			m_obj(o.m_obj),
+			m_func(o.m_func) {
+	}
+
+	Callback& operator=(const Callback &o) {
+		m_obj = o.m_obj;
+		m_func = o.m_func;
+		return *this;
+	}
+
+	explicit operator bool() {
+		return m_func != 0 ? m_obj : 0;
+	}
+
+	R operator()(ARGS ... args) {
+		return m_func(m_obj, args...);
+	}
 
 private:
-    typedef const void* Callback::*SafeBoolType;
-public:
-    inline operator SafeBoolType() const
-        { return func != 0 ? &Callback::obj : 0; }
-    inline bool operator!() const
-        { return func == 0; }
 
-private:
-    typedef R (*FuncType)(const void*, Args...);
-    Callback(FuncType f, const void* o) : func(f), obj(o) {}
-
-private:
-    FuncType func;
-    const void* obj;
-
-    template<typename FR, typename ...FA>
-    friend class FreeCallbackFactory;
-
-    template<typename FR, typename FT, typename ...FA>
-    friend class MemberCallbackFactory;
-
-    template<typename FR, typename FT, typename ...FA>
-    friend class ConstMemberCallbackFactory;
+	void *m_obj;
+	function m_func;
 };
 
-template<typename R, typename... ARGS>
-void operator==(const Callback<R (ARGS...)>&,
-                const Callback<R (ARGS...)>&);
-template<typename R, typename... ARGS>
-void operator!=(const Callback<R (ARGS...)>&,
-                const Callback<R (ARGS...)>&);
+namespace Factory {
 
-template<typename R, typename... ARGS>
-class FreeCallbackFactory
-{
-private:
-    template<R (*Func)(ARGS...)>
-    static R Wrapper(const void*, ARGS... args)
-    {
-        return (*Func)(args...);
-    }
-
-public:
-    template<R (*Func)(ARGS...)>
-    inline static Callback<R (ARGS...)> Bind()
-    {
-        return Callback<R (ARGS...)>
-            (&FreeCallbackFactory::Wrapper<Func>, 0);
-    }
+template<class T, typename R, typename ... ARGS>
+struct MemberFunc {
+	template<R (T::*Func)(ARGS...)>
+	static inline Callback<R(ARGS...)> bind(T *t) {
+		struct _ {
+			static R wrapper(void *obj, ARGS ... args) {
+				return (static_cast<T*>(obj)->*Func)(
+						std::forward<ARGS>(args)...);
+			}
+		};
+		return Callback<R(ARGS...)>(t, _::wrapper);
+	}
 };
 
-template<typename R, typename... ARGS>
-inline FreeCallbackFactory<R, ARGS...>
-GetCallbackFactory(R (*)(ARGS...))
-{
-    return FreeCallbackFactory<R, ARGS...>();
+template<class T, typename R, typename ... ARGS>
+struct ConstMemberFunc {
+	template<R (T::*Func)(ARGS...)>
+	static inline Callback<R(ARGS...)> bind(T *t) {
+		struct _ {
+			static R wrapper(void *obj, ARGS ... args) {
+				return (static_cast<T*>(obj)->*Func)(
+						std::forward<ARGS>(args)...);
+			}
+		};
+		return Callback<R(ARGS...)>(t, _::wrapper);
+	}
+};
+
+template<typename R, typename ... ARGS>
+struct FreeFunc {
+	template<R (*Func)(ARGS...)>
+	static inline Callback<R(ARGS...)> bind() {
+		struct _ {
+			static R wrapper(void *obj, ARGS ... args) {
+				return Func(std::forward<ARGS>(args)...);
+			}
+		};
+		return Callback<R(ARGS...)>(nullptr, _::wrapper);
+	}
+};
+
+template<class T, typename R, typename ... ARGS>
+inline MemberFunc<T, R, ARGS...> get(R (T::*)(ARGS...)) {
+	return MemberFunc<T, R, ARGS...>();
 }
 
-template<typename R, class T, typename... ARGS>
-class MemberCallbackFactory
-{
-private:
-    template<R (T::*Func)(ARGS...)>
-    static R Wrapper(const void* o, ARGS... args)
-    {
-        T* obj = const_cast<T*>(static_cast<const T*>(o));
-        return (obj->*Func)(args...);
-    }
-
-public:
-    template<R (T::*Func)(ARGS...)>
-    inline static Callback<R (ARGS...)> Bind(T* o)
-    {
-        return Callback<R (ARGS...)>
-        	(&MemberCallbackFactory::Wrapper<Func>,
-            static_cast<const void*>(o));
-    }
-};
-
-template<typename R, class T, typename... ARGS>
-inline MemberCallbackFactory<R, T, ARGS...>
-GetCallbackFactory(R (T::*)(ARGS...))
-{
-    return MemberCallbackFactory<R, T, ARGS...>();
+template<class T, typename R, typename ... ARGS>
+inline ConstMemberFunc<T, R, ARGS...> get(R (T::*)(ARGS...) const) {
+	return ConstMemberFunc<T, R, ARGS...>();
 }
 
-template<typename R, class T, typename... ARGS>
-class ConstMemberCallbackFactory
-{
-private:
-    template<R (T::*Func)(ARGS...) const>
-    static R Wrapper(const void* o, ARGS... args)
-    {
-        const T* obj = static_cast<const T*>(o);
-        return (obj->*Func)(args...);
-    }
-
-public:
-    template<R (T::*Func)(ARGS...) const>
-    inline static Callback<R (ARGS...)> Bind(const T* o)
-    {
-        return Callback<R (ARGS...)>
-            (&ConstMemberCallbackFactory::Wrapper<Func>,
-            static_cast<const void*>(o));
-    }
-};
-
-template<typename R, class T, typename... ARGS>
-inline ConstMemberCallbackFactory<R, T, ARGS...>
-GetCallbackFactory(R (T::*)(ARGS...) const)
-{
-    return ConstMemberCallbackFactory<R, T, ARGS...>();
+template<typename R, typename ... ARGS>
+inline FreeFunc<R, ARGS...> get(R (*)(ARGS...)) {
+	return FreeFunc<R, ARGS...>();
 }
 
 }
 
-using callback_detail::Callback;
+}
 
-#endif
+#define CD_BIND_MEM_IMPL(instance, func) \
+(CB::Factory::get(func).template bind<func>(instance))
+#define CB_BIND_MEM CD_BIND_MEM_IMPL
 
+#define CD_BIND_FREE_IMPL(func) \
+(CB::Factory::get(func).template bind<func>())
+#define CB_BIND_FREE CD_BIND_FREE_IMPL
+
+#endif /* CALLBACK_HPP_ */
